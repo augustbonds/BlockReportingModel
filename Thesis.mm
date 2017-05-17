a67a3d349f72e4b100670508e4e71f6df1b93843
August Bonds <august.bonds@protonmail.com>
2017-05-17 16:12:41 +0200

    adding mind map over interesting topics to include in Thesis

---
+++ Thesis.mm
@@ -0,0 +1,106 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Thesis" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1495028004222"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="" POSITION="right" ID="ID_692659868" CREATED="1495027870867" MODIFIED="1495027870868">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="Future Work" POSITION="left" ID="ID_1568823250" CREATED="1495029970026" MODIFIED="1495029995219">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Conclusion" ID="ID_162958207" CREATED="1495028755948" MODIFIED="1495030002758">
+<font BOLD="false"/>
+<node TEXT="Evaluation" ID="ID_568825307" CREATED="1495028055745" MODIFIED="1495029467371">
+<font BOLD="false"/>
+<node TEXT="Methodology" ID="ID_877910286" CREATED="1495028080521" MODIFIED="1495029469163">
+<font BOLD="false"/>
+<node TEXT="Simulation" ID="ID_648694751" CREATED="1495028526869" MODIFIED="1495028530362"/>
+<node TEXT="Empiric" ID="ID_1318422852" CREATED="1495028531990" MODIFIED="1495028536882"/>
+<node TEXT="Quantitative" ID="ID_1258377511" CREATED="1495028537517" MODIFIED="1495028564034"/>
+</node>
+<node TEXT="Experiments" ID="ID_1906912881" CREATED="1495028096633" MODIFIED="1495028810721">
+<node TEXT="Old Block Report" ID="ID_1534389546" CREATED="1495028814035" MODIFIED="1495028824375"/>
+<node TEXT="Scenarios" ID="ID_501733579" CREATED="1495028129129" MODIFIED="1495028147149">
+<node TEXT="1% failure" ID="ID_1153941663" CREATED="1495028503198" MODIFIED="1495028511891"/>
+<node TEXT="Cluster Starup" ID="ID_1409899936" CREATED="1495029274032" MODIFIED="1495029282268"/>
+<node TEXT="Massive Block Migration" ID="ID_110069792" CREATED="1495029283503" MODIFIED="1495029289740"/>
+<node TEXT="Different Write Loads" ID="ID_1618128502" CREATED="1495030129481" MODIFIED="1495030234612">
+<node TEXT="Temporary NN Slowdown" ID="ID_29382831" CREATED="1495028512790" MODIFIED="1495029272765"/>
+<node TEXT="Infinte NN capacity" ID="ID_1338400212" CREATED="1495030261055" MODIFIED="1495030267388"/>
+<node TEXT="Limited #NN" ID="ID_1183998357" CREATED="1495030273799" MODIFIED="1495030286988"/>
+</node>
+</node>
+<node TEXT="Hashed Block Report" ID="ID_582831190" CREATED="1495028174840" MODIFIED="1495028905879">
+<node TEXT="HopsFS" ID="ID_1316955380" CREATED="1495028356095" MODIFIED="1495028362331">
+<node TEXT="HDFS" ID="ID_1834577332" CREATED="1495028363359" MODIFIED="1495028366891">
+<node TEXT="Hadoop" ID="ID_1417768336" CREATED="1495028368271" MODIFIED="1495028370563"/>
+<node TEXT="GFS" ID="ID_1648654166" CREATED="1495029929450" MODIFIED="1495029931703"/>
+</node>
+<node TEXT="NDB" ID="ID_407257519" CREATED="1495028376511" MODIFIED="1495028379283"/>
+<node TEXT="Transactions" ID="ID_482301277" CREATED="1495028389591" MODIFIED="1495028393515"/>
+</node>
+<node TEXT="Requirements" ID="ID_1768829318" CREATED="1495029508902" MODIFIED="1495029510930">
+<node TEXT="Old Block Report Guarantees" ID="ID_435946317" CREATED="1495029517429" MODIFIED="1495029533753"/>
+<node TEXT="Speed" ID="ID_818784003" CREATED="1495029552421" MODIFIED="1495029827919"/>
+<node TEXT="Scalability" ID="ID_395386022" CREATED="1495029834187" MODIFIED="1495029836263"/>
+</node>
+<node TEXT="Architecture" ID="ID_1188038994" CREATED="1495029643573" MODIFIED="1495029646544">
+<node TEXT="Incremental Reports" ID="ID_1828579699" CREATED="1495028617533" MODIFIED="1495029064214"/>
+<node TEXT="Bucketized Reports" ID="ID_1798293028" CREATED="1495029156608" MODIFIED="1495029187205"/>
+<node TEXT="Hashing" ID="ID_1795306996" CREATED="1495028682932" MODIFIED="1495028693089"/>
+<node TEXT="Bucketizing" ID="ID_1025574804" CREATED="1495028699540" MODIFIED="1495028701761"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>

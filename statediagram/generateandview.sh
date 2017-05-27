0a7e6477dbfd9e19ff3418d60984ef17ac6f0186
August Bonds <august.bonds@protonmail.com>
2017-05-27 16:21:46 +0200

    updating graph generation script and ignoring image files

--- statediagram/generateandview.sh
+++ statediagram/generateandview.sh
@@ -1,5 +1,19 @@
-#GRAPH_CMD=dot
-GRAPH_CMD=neato
-INFILE=replica_states.dot
+if [ $# -eq 0 ]
+  then
+    echo "usage: ./generateandview.sh <graph engine>"
+    exit 1
+fi
 
-$GRAPH_CMD -Tps $INFILE | gv -
+case $1 in 
+  dot|neato|twopi)
+    echo "Valid graph engine" ;;
+  *)
+    echo "Not a valid graph engine"
+    exit 1 ;;
+esac
+
+file=replica_states.dot
+
+dot -Tpng -O $file
+
+feh $file.png

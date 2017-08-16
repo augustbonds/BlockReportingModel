5cd6f990ebbe611d4e8d22b9fcb13de4da08f4de
August Bonds <august.bonds@protonmail.com>
2017-08-16 22:10:59 +0200

    update freeplane and add TODO

--- Thesis.mm
+++ Thesis.mm
@@ -1,21 +1,26 @@
-<map version="freeplane 1.2.0">
+<map version="freeplane 1.6.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Thesis" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1495028004222"><hook NAME="MapStyle">
+<node TEXT="Thesis" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1495028004222"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false"/>
 
 <map_styles>
-<stylenode LOCALIZED_TEXT="styles.root_node">
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -29,7 +34,7 @@
 <icon BUILTIN="yes"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
 <font SIZE="18"/>
 </stylenode>
@@ -49,7 +54,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="" POSITION="right" ID="ID_692659868" CREATED="1495027870867" MODIFIED="1495027870868">
 <edge COLOR="#ffff00"/>
 </node>
@@ -65,7 +70,7 @@
 <node TEXT="Empiric" ID="ID_1318422852" CREATED="1495028531990" MODIFIED="1495028536882"/>
 <node TEXT="Quantitative" ID="ID_1258377511" CREATED="1495028537517" MODIFIED="1495028564034"/>
 </node>
-<node TEXT="Experiments" ID="ID_1906912881" CREATED="1495028096633" MODIFIED="1495028810721">
+<node TEXT="Experiments" ID="ID_1906912881" CREATED="1495028096633" MODIFIED="1502434278491">
 <node TEXT="Old Block Report" ID="ID_1534389546" CREATED="1495028814035" MODIFIED="1495028824375"/>
 <node TEXT="Scenarios" ID="ID_501733579" CREATED="1495028129129" MODIFIED="1495028147149">
 <node TEXT="1% failure" ID="ID_1153941663" CREATED="1495028503198" MODIFIED="1495028511891"/>
@@ -77,8 +82,8 @@
 <node TEXT="Limited #NN" ID="ID_1183998357" CREATED="1495030273799" MODIFIED="1495030286988"/>
 </node>
 </node>
-<node TEXT="Hashed Block Report" ID="ID_582831190" CREATED="1495028174840" MODIFIED="1495028905879">
-<node TEXT="HopsFS" ID="ID_1316955380" CREATED="1495028356095" MODIFIED="1495028362331">
+<node TEXT="Hashed Block Report" ID="ID_582831190" CREATED="1495028174840" MODIFIED="1502434270653">
+<node TEXT="HopsFS" FOLDED="true" ID="ID_1316955380" CREATED="1495028356095" MODIFIED="1495028362331">
 <node TEXT="HDFS" ID="ID_1834577332" CREATED="1495028363359" MODIFIED="1495028366891">
 <node TEXT="Hadoop" ID="ID_1417768336" CREATED="1495028368271" MODIFIED="1495028370563"/>
 <node TEXT="GFS" ID="ID_1648654166" CREATED="1495029929450" MODIFIED="1495029931703"/>
@@ -87,7 +92,9 @@
 <node TEXT="Transactions" ID="ID_482301277" CREATED="1495028389591" MODIFIED="1495028393515"/>
 </node>
 <node TEXT="Requirements" ID="ID_1768829318" CREATED="1495029508902" MODIFIED="1495029510930">
-<node TEXT="Old Block Report Guarantees" ID="ID_435946317" CREATED="1495029517429" MODIFIED="1495029533753"/>
+<node TEXT="Old Block Report Guarantees" ID="ID_435946317" CREATED="1495029517429" MODIFIED="1495029533753">
+<node TEXT="Eventual Consistency" ID="ID_230187527" CREATED="1502434332389" MODIFIED="1502434342504"/>
+</node>
 <node TEXT="Speed" ID="ID_818784003" CREATED="1495029552421" MODIFIED="1495029827919"/>
 <node TEXT="Scalability" ID="ID_395386022" CREATED="1495029834187" MODIFIED="1495029836263"/>
 </node>
